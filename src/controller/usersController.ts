61f9dca57381b2e46f2703187cc3e7dce967fe26
Dayanis <dayany1588@yahoo.com>
2024-03-21 01:44:01 +0100

    Se ha creado el archivo authController

--- src/controller/usersController.ts
+++ src/controller/usersController.ts
@@ -1,33 +1,170 @@
-import { User } from "../models/user";
-import { Response, Request } from "express";
-import { AppDataSource } from "../database/data-source";
+import { Request, Response } from "express";
+import AppDataSource from "../database/data-source";
+import { User, Role } from "../models/user";
+import { StatusCodes } from "http-status-codes";
 
 export class UsersController {
-    async getAll(req: Request, res: Response) {
-        const users = await AppDataSource.getRepository(User).find();
-        return res.json(users);
-    }
-
-    //Recuperar un usuario por ID
-    async getById(req: Request, res: Response) {
-        const id = parseInt(req.params.id);
-
-        if (isNaN(id)) {
-            return res.status(400).json({
-                message: "Invalid ID"
+    async getAllUsers(req: Request, res: Response): Promise<void> {
+        const userRepository = AppDataSource.getRepository(User);
+        try {
+            const users = await userRepository.find();
+            res.status(StatusCodes.OK).json(users);
+        } catch (error) {
+            console.error(error);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message: "Error retrieving users",
             });
         }
+    }
 
-        const user = await AppDataSource.getRepository(User).findOne({
-            where: { id }
-        });
+    async getUserById(req: Request, res: Response): Promise<void> {
+        const userRepository = AppDataSource.getRepository(User);
+        const { id } = req.params;
 
-        if (!user) {
-            return res.status(404).json({
-                message: "User not found"
+        try {
+            const user = await userRepository.findOneBy({ id: parseInt(id) });
+            if (user) {
+                res.status(StatusCodes.OK).json(user);
+            } else {
+                res.status(StatusCodes.NOT_FOUND).json({
+                    message: "User not found",
+                });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message: "Error retrieving user",
             });
         }
-
-        return res.json(user);
     }
+
+    async createUser(req: Request, res: Response): Promise<void> {
+        const userRepository = AppDataSource.getRepository(User);
+        const newUser = userRepository.create(req.body);
+
+        try {
+            await userRepository.save(newUser);
+            res.status(StatusCodes.CREATED).json(newUser);
+        } catch (error) {
+            console.error(error);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message: "Error creating user",
+            });
+        }
+    }
+
+    async updateUser(req: Request, res: Response): Promise<void> {
+        const userRepository = AppDataSource.getRepository(User);
+        const { id } = req.params;
+        const updateData = req.body;
+
+        try {
+            const user = await userRepository.findOneBy({ id: parseInt(id) });
+            if (user) {
+                userRepository.merge(user, updateData);
+                const result = await userRepository.save(user);
+                res.status(StatusCodes.OK).json(result);
+            } else {
+                res.status(StatusCodes.NOT_FOUND).json({
+                    message: "User not found",
+                });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message: "Error updating user",
+            });
+        }
+    }
+
+    async deleteUser(req: Request, res: Response): Promise<void> {
+        const userRepository = AppDataSource.getRepository(User);
+        const { id } = req.params;
+
+        try {
+            const result = await userRepository.delete(id);
+            if (result) {
+                res.status(StatusCodes.OK).json({
+                    message: "User deleted successfully",
+                });
+            } else {
+                res.status(StatusCodes.NOT_FOUND).json({
+                    message: "User not found",
+                });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message: "Error deleting user",
+            });
+        }
+    }
+
+    async listStudents(req: Request, res: Response): Promise<void> {
+        const userRepository = AppDataSource.getRepository(User);
+        const students = await userRepository.find({ where: { rol: Role.student } });
+        res.json(students);
+    }
+    
+    async studentById(req: Request, res: Response): Promise<void> {
+        const { id } = req.params; // Obtén el ID del parámetro de la ruta
+        const userRepository = AppDataSource.getRepository(User);
+    
+        try {
+            // Intenta encontrar un usuario con el rol 'student' y el ID proporcionado
+            const student = await userRepository.findOneBy({
+                id: parseInt(id),
+                rol: Role.student
+            });
+    
+            if (student) {
+                // Si se encuentra el estudiante, devolver los datos del estudiante
+                res.status(StatusCodes.OK).json(student);
+            } else {
+                // Si no se encuentra el estudiante, devolver un error 404
+                res.status(StatusCodes.NOT_FOUND).json({
+                    message: "Student not found",
+                });
+            }
+        } catch (error) {            
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message: "Error retrieving student",
+            });
+        }
+    }
+
+    async listTeachers(req: Request, res: Response): Promise<void> {
+        const userRepository = AppDataSource.getRepository(User);
+        const teachers = await userRepository.find({ where: { rol: Role.teacher } });
+        res.json(teachers);
+    }
+
+    async teacherById(req: Request, res: Response): Promise<void> {
+        const { id } = req.params; // Obtén el ID del parámetro de la ruta
+        const userRepository = AppDataSource.getRepository(User);
+    
+        try {
+            // Intenta encontrar un usuario con el rol 'student' y el ID proporcionado
+            const teacher = await userRepository.findOneBy({
+                id: parseInt(id),
+                rol: Role.teacher
+            });
+    
+            if (teacher) {
+                // Si se encuentra el estudiante, devolver los datos del estudiante
+                res.status(StatusCodes.OK).json(teacher);
+            } else {
+                // Si no se encuentra el estudiante, devolver un error 404
+                res.status(StatusCodes.NOT_FOUND).json({
+                    message: "Student not found",
+                });
+            }
+        } catch (error) {
+            // Si hay un error durante la recuperación del usuario, devolver un error 500
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message: "Error retrieving student",
+            });
+        }
+    }
+    
 }
